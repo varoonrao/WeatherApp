c80827da159cbf7a6a6049665cf2b6477e026ae8
varoonrao <varunrao72@gmail.com>
2019-10-01 11:42:33 +0530

    11:42

---
+++ script.js
@@ -0,0 +1,7 @@
+class Weather{
+    constructor(temperature_max, temperature_min, current_place){
+        this.temperature_max=max_temperature;
+        this.temperature_min=min_temperature;
+        this.current_place=place;
+    }
+}  
--- weather.js
+++ weather.js
@@ -5,9 +5,10 @@ const tempElememnt = document.querySelector(".temperature p");
 const rightElement = document.querySelector(".right p");
 const cityElement = document.querySelector(".right h4");
 const tempminElement = document.querySelector(".temperature1 p");
-//App data
+const weather_image_full = document.querySelector(".image_div");
+
 const weather = {};
-// const forecast={};
+
 weather.temperature = {
     unit: "celsius"
 }
@@ -15,10 +16,6 @@ weather.temperature1 = {
     unit: "celsius"
 }
 
-//App const and vars
-const KELVIN = 273;
-//api key
-const key = "3925448c90c02ec2a83ed41c1a713a1d";
 
 //geolocation supports
 if ('geolocation' in navigator) {
@@ -44,7 +41,7 @@ function showError(error) {
 
 //get weather from api
 function getWeather(latitude, longitude) {
-    let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+    let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&APPID=3925448c90c02ec2a83ed41c1a713a1d`;
     fetch(api)
         .then(function (response) {
             let data = response.json();
@@ -55,29 +52,24 @@ function getWeather(latitude, longitude) {
             weather.description = data.dt;
             weather.hour = data.dt;
             weather.iconId = data.weather[0].icon;
-            weather.temperature.value = Math.floor(data.main.temp_max - KELVIN);
+            weather.temperature.value = Math.floor(data.main.temp_max);
             weather.city = data.name;
-            weather.temperature1.value = Math.floor(data.main.temp_min - KELVIN);
+            weather.temperature1.value = Math.floor(data.main.temp_min);
             var dt = new Date((weather.description) * 1000);
-            var weekday = new Array(7);
-            weekday[0] = "Sunday";
-            weekday[1] = "Monday";
-            weekday[2] = "Tuesday";
-            weekday[3] = "Wednesday";
-            weekday[4] = "Thursday";
-            weekday[5] = "Friday";
-            weekday[6] = "Saturday";
+            var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "thursday", "Friday", "saturday"];
             dayElememnt.innerHTML = weekday[dt.getDay()];
             var hours = new Date((weather.hour) * 1000);
             var hour = hours.getHours();
             if (hour > 0 && hour < 12) {
                 rightElement.innerHTML = "GOOD MORNING";
+                weather_image_full.innerHTML = `<img src="imgs/morning.jpg" style="width:541px;height:280px;">`;
             } else if (hour >= 12 && hour < 16) {
                 rightElement.innerHTML = "GOOD AFTERNOON";
-            } else if (hour >= 16 && hour < 19) {
-                rightElement.innerHTML = "GOOD EVENING";
+                weather_image_full.innerHTML = "<img src='imgs/afternoon.jpg' style='width:541px;height:280px;'>";
             } else {
-                rightElement.innerHTML = "GOOD NIGHT";
+                rightElement.innerHTML = "GOOD EVENING";
+                weather_image_full.innerHTML = "<img src='imgs/evening.jpg' style='width:541px;height:280px;'>";
+
             }
 
         })
@@ -93,7 +85,7 @@ function onClickMenu() {
 }
 
 function cityClick(latitude, longitude, time) {
-    let api1 = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+    let api1 = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&APPID=3925448c90c02ec2a83ed41c1a713a1d`;
 
     fetch(api1)
         .then(function (response) {
@@ -105,30 +97,24 @@ function cityClick(latitude, longitude, time) {
             weather.description = data1.dt;
             weather.hour = data1.dt;
             weather.iconId = data1.weather[0].icon; //
-            weather.temperature.value = Math.floor(data1.main.temp_max - KELVIN); //
+            weather.temperature.value = Math.floor(data1.main.temp_max); //
             weather.city = data1.name; //
-            weather.temperature1.value = Math.floor(data1.main.temp_min - KELVIN); //
+            weather.temperature1.value = Math.floor(data1.main.temp_min); //
             var d = new Date((weather.description) * 1000);
-            var weekday = new Array(7);
-            weekday[0] = "Sunday";
-            weekday[1] = "Monday";
-            weekday[2] = "Tuesday";
-            weekday[3] = "Wednesday";
-            weekday[4] = "Thursday";
-            weekday[5] = "Friday";
-            weekday[6] = "Saturday";
+            var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "thursday", "Friday", "saturday"];
             dayElememnt.innerHTML = weekday[d.getDay()];
             var hours1 = new Date((weather.hour) * 1000);
             var hour0 = hours1.getHours();
             hour1 = hour0 - time;
             if (hour1 > 0 && hour1 < 12) {
                 rightElement.innerHTML = "GOOD MORNING";
+                weather_image_full.innerHTML = "<img src='imgs/morning.jpg' style='width:541px;height:280px;'>";
             } else if (hour1 >= 12 && hour1 < 16) {
                 rightElement.innerHTML = "GOOD AFTERNOON";
-            } else if (hour1 >= 16 && hour1 < 19) {
-                rightElement.innerHTML = "GOOD EVENING";
+                weather_image_full.innerHTML = "<img src='imgs/afternoon.jpg' style='width:541px;height:280px;'>";
             } else {
-                rightElement.innerHTML = "GOOD NIGHT";
+                rightElement.innerHTML = "GOOD EVENING";
+                weather_image_full.innerHTML = "<img src='imgs/evening.jpg' style='width:541px;height:280px;'>";
             }
 
         })
@@ -139,12 +125,12 @@ function cityClick(latitude, longitude, time) {
 
 function displayWeather() {
     iconElememnt.innerHTML = `<img src="icons/${weather.iconId}.png"/>`;
-    tempElememnt.innerHTML = `${weather.temperature.value}&#176 C |`;
-    tempminElement.innerHTML = `${weather.temperature1.value}&#176 C`;  
+    tempElememnt.innerHTML = `${weather.temperature.value}&deg; C |`;
+    tempminElement.innerHTML = `${weather.temperature1.value}&deg; C`;
     cityElement.innerHTML = weather.city;
 }
 
-// ?forecast
+// forecast
 
 
 const dayElememnt1 = document.querySelector(".day1");
@@ -182,7 +168,7 @@ forecast.temperature = {
 }
 
 function getForecast(latitude, longitude) {
-    let api2 = `http://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${key}`;
+    let api2 = `http://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=metric&APPID=3925448c90c02ec2a83ed41c1a713a1d`;
     // console.log(api2);
     fetch(api2)
         .then(function (response) {
@@ -197,19 +183,19 @@ function getForecast(latitude, longitude) {
             forecast.day5 = data2.list[32].dt_txt;
             forecast.day6 = data2.list[39].dt_txt;
 
-            forecast.temperature.value2 = Math.floor(data2.list[8].main.temp_min - KELVIN);
-            forecast.temperature.value3 = Math.floor(data2.list[16].main.temp_min - KELVIN);
-            forecast.temperature.value4 = Math.floor(data2.list[24].main.temp_min - KELVIN);
-            forecast.temperature.value5 = Math.floor(data2.list[32].main.temp_min - KELVIN);
-            forecast.temperature.value6 = Math.floor(data2.list[39].main.temp_min - KELVIN);
+            forecast.temperature.value2 = Math.floor(data2.list[8].main.temp_min);
+            forecast.temperature.value3 = Math.floor(data2.list[16].main.temp_min);
+            forecast.temperature.value4 = Math.floor(data2.list[24].main.temp_min);
+            forecast.temperature.value5 = Math.floor(data2.list[32].main.temp_min);
+            forecast.temperature.value6 = Math.floor(data2.list[39].main.temp_min);
 
-            forecast.temperature.value21 = Math.floor(data2.list[8].main.temp_max - KELVIN);
+            forecast.temperature.value21 = Math.floor(data2.list[8].main.temp_max);
             // console.log(forecast.temperature.value21);
 
-            forecast.temperature.value31 = Math.floor(data2.list[16].main.temp_max - KELVIN);
-            forecast.temperature.value41 = Math.floor(data2.list[24].main.temp_max - KELVIN);
-            forecast.temperature.value51 = Math.floor(data2.list[32].main.temp_max - KELVIN);
-            forecast.temperature.value61 = Math.floor(data2.list[39].main.temp_max - KELVIN);
+            forecast.temperature.value31 = Math.floor(data2.list[16].main.temp_max);
+            forecast.temperature.value41 = Math.floor(data2.list[24].main.temp_max);
+            forecast.temperature.value51 = Math.floor(data2.list[32].main.temp_max);
+            forecast.temperature.value61 = Math.floor(data2.list[39].main.temp_max);
             // console.log(forecast.temperature.value);
 
             forecast.ic1 = data2.list[0].weather[0].icon;
@@ -228,16 +214,7 @@ function getForecast(latitude, longitude) {
 }
 
 function displayForecast() {
-    var weekday = new Array(7);
-    weekday[0] = "Sunday";
-    weekday[1] = "Monday";
-    weekday[2] = "Tuesday";
-    weekday[3] = "Wednesday";
-    weekday[4] = "Thursday";
-    weekday[5] = "Friday";
-    weekday[6] = "Saturday";
-    // dayElememnt.innerHTML=weekday[d.getDay()];
-
+    var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "thursday", "Friday", "saturday"];
     var a = new Date(forecast.day1);
     dayElememnt1.innerHTML = weekday[a.getDay()];
     var b = new Date(forecast.day2);
@@ -251,19 +228,19 @@ function displayForecast() {
     var f = new Date(forecast.day6);
     dayElememnt6.innerHTML = weekday[f.getDay()];
 
-    mintemp1.innerHTML = `${weather.temperature.value}&#176 C |`;
-    mintemp2.innerHTML = `${forecast.temperature.value2}&#176 C |`;
-    mintemp3.innerHTML = `${forecast.temperature.value3}&#176 C |`;
-    mintemp4.innerHTML = `${forecast.temperature.value4}&#176 C |`;
-    mintemp5.innerHTML = `${forecast.temperature.value5}&#176 C |`;
-    mintemp6.innerHTML = `${forecast.temperature.value6}&#176 C |`;
+    mintemp1.innerHTML = `${weather.temperature.value}&deg; C |`;
+    mintemp2.innerHTML = `${forecast.temperature.value2}&deg; C |`;
+    mintemp3.innerHTML = `${forecast.temperature.value3}&deg; C |`;
+    mintemp4.innerHTML = `${forecast.temperature.value4}&deg; C |`;
+    mintemp5.innerHTML = `${forecast.temperature.value5}&deg; C |`;
+    mintemp6.innerHTML = `${forecast.temperature.value6}&deg; C |`;
 
-    maxtemp1.innerHTML = `${weather.temperature1.value}&#176 C `;
-    maxtemp2.innerHTML = `${forecast.temperature.value21}&#176 C `;
-    maxtemp3.innerHTML = `${forecast.temperature.value31}&#176 C `;
-    maxtemp4.innerHTML = `${forecast.temperature.value41}&#176 C `;
-    maxtemp5.innerHTML = `${forecast.temperature.value51}&#176 C `;
-    maxtemp6.innerHTML = `${forecast.temperature.value61}&#176 C `;
+    maxtemp1.innerHTML = `${weather.temperature1.value}&deg; C `;
+    maxtemp2.innerHTML = `${forecast.temperature.value21}&deg; C `;
+    maxtemp3.innerHTML = `${forecast.temperature.value31}&deg; C `;
+    maxtemp4.innerHTML = `${forecast.temperature.value41}&deg; C `;
+    maxtemp5.innerHTML = `${forecast.temperature.value51}&deg; C `;
+    maxtemp6.innerHTML = `${forecast.temperature.value61}&deg; C `;
 
     iconone.innerHTML = `<img class="icon_down" src="icon/${forecast.ic1}.png"/>`;
     icontwo.innerHTML = `<img class="icon_down" src="icon/${forecast.ic2}.png"/>`;
@@ -273,6 +250,3 @@ function displayForecast() {
     iconsix.innerHTML = `<img class="icon_down" src="icon/${forecast.ic6}.png"/>`;
 
 }
-// tempElememnt.innerHTML=`${weather.temperature.value}&#176`;
-// dayElememnt.innerHTML=weekday[d.getDay()];
-//
